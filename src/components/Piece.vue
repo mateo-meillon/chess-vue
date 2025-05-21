f6a8b013c1d91466bf64f5b8fa81b615db25d421
mateo-meillon <matev3@outlook.de>
2025-05-22 00:19:14 +0200

    Add chess piece SVGs and update Piece component to use imports

--- src/components/Piece.vue
+++ src/components/Piece.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+import klt45 from '../assets/pieces/Chess_klt45.svg'
+import qlt45 from '../assets/pieces/Chess_qlt45.svg'
+import rlt45 from '../assets/pieces/Chess_rlt45.svg'
+import blt45 from '../assets/pieces/Chess_blt45.svg'
+import nlt45 from '../assets/pieces/Chess_nlt45.svg'
+import plt45 from '../assets/pieces/Chess_plt45.svg'
+import kdt45 from '../assets/pieces/Chess_kdt45.svg'
+import qdt45 from '../assets/pieces/Chess_qdt45.svg'
+import rdt45 from '../assets/pieces/Chess_rdt45.svg'
+import bdt45 from '../assets/pieces/Chess_bdt45.svg'
+import ndt45 from '../assets/pieces/Chess_ndt45.svg'
+import pdt45 from '../assets/pieces/Chess_pdt45.svg'
+
 const props = defineProps<{
 	type: string // e.g., 'wK', 'bQ', etc.
 }>()
@@ -7,26 +20,23 @@ const emit = defineEmits(['mousedown', 'touchstart'])
 
 // Map piece codes to SVG filenames
 const pieceMap: Record<string, string> = {
-	wK: 'Chess_klt45.svg',
-	wQ: 'Chess_qlt45.svg',
-	wR: 'Chess_rlt45.svg',
-	wB: 'Chess_blt45.svg',
-	wN: 'Chess_nlt45.svg',
-	wP: 'Chess_plt45.svg',
-	bK: 'Chess_kdt45.svg',
-	bQ: 'Chess_qdt45.svg',
-	bR: 'Chess_rdt45.svg',
-	bB: 'Chess_bdt45.svg',
-	bN: 'Chess_ndt45.svg',
-	bP: 'Chess_pdt45.svg',
+	wK: klt45,
+	wQ: qlt45,
+	wR: rlt45,
+	wB: blt45,
+	wN: nlt45,
+	wP: plt45,
+	bK: kdt45,
+	bQ: qdt45,
+	bR: rdt45,
+	bB: bdt45,
+	bN: ndt45,
+	bP: pdt45,
 }
-
-const route = import.meta.env.BASE_URL
-const src = `url(${route}pieces/${pieceMap[props.type]})`
 </script>
 
 <template>
-	<img v-if="pieceMap[type]" :src="src" :alt="type" class="piece" draggable="false" @mousedown="(e) => emit('mousedown', e)" @touchstart="(e) => emit('touchstart', e)" />
+	<img v-if="pieceMap[type]" :src="pieceMap[type]" :alt="type" class="piece" draggable="false" @mousedown="(e) => emit('mousedown', e)" @touchstart="(e) => emit('touchstart', e)" />
 </template>
 
 <style scoped>
